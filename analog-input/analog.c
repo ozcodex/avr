a0dc09da699914b9e303b850c9d90cc5dee5c378
Oscar Bernal <oscarbernal93@gmail.com>
2020-10-11 01:59:13 -0500

    Enable analog reading
    Signed-off-by: Oscar Bernal <oscarbernal93@gmail.com>

--- analog-input/analog.c
+++ analog-input/analog.c
@@ -19,12 +19,9 @@ int main(void) {
   while( 1 ){
     // Read Analog Data
     // 0100 0000 -> 0x40 ADSC Bit (start conversion)
-    //ADCSRA |= 0x40;
-    //byte = ADCH;
-    for (i=0;i<255;i++){
-      putHex(i);
-      _delay_ms(100);
-    }
+    ADCSRA |= 0x40;
+    byte = ADCH;
+    putHex(byte);
     _delay_ms(100);
   }
   return 0;
@@ -40,19 +37,19 @@ void setup() {
   // 0X00 0000 refs0 Voltage Analog Reference, internal or vcc (0:Vcc,1:internal)
   // 0010 0000 ADLAR for 8 bit pressision instead of 10 bit
   // 0000 00XX Analog Pin 01:pb2 10:pb4 11:pb3
-  // 0110 0001 -> 0x61
-  //ADMUX |= 0x61;
+  // 0010 0001 -> 0x21
+  ADMUX |= 0x21;
   // Setup Analog Read Method ADCSRA Register
   // (Analog to Digital Converter Control and Status Register A)
   // 1000 0000 ADC Enable
   // 0000 0110 Prescaler: 2^n to divide clock freq and keep it in 50-200 kHz
   // 1000 0110 -> 0x86
-  //ADCSRA |= 0x86;
+  ADCSRA |= 0x86;
 }
 
 
 void putHex(uint8_t byte){
-  putWord( hex[ ((byte) >> 4) & 0xF0 ], hex[ (byte & 0x0F) ] );
+  putWord( hex[ ((byte & 0xF0) >> 4) ], hex[ (byte & 0x0F) ] );
 }
 
 void putWord(uint8_t first, uint8_t last){
