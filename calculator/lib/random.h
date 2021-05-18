7cc9f2c52ca0bbeb5f13510e361b70781cc966d0
Oscar Bernal <oscarbernal93@gmail.com>
2021-05-17 22:32:53 -0500

    Add random generator lib

---
+++ calculator/lib/random.h
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2017, Łukasz Marcin Podkalicki <lpodkalicki@gmail.com>
+ * Lightweight library of 16 bit random number generator based on LFSR.
+ */
+
+#ifndef _RANDOM_H_
+#define	_RANDOM_H_
+
+void random_init(uint16_t seed);
+uint16_t random(void);
+
+#endif	/* !_RANDOM_H_ */
+
