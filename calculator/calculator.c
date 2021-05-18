f9b0b3059b70d5d16b1eb8aa1f52edb20565acd4
Oscar Bernal <oscarbernal93@gmail.com>
2021-05-17 23:16:32 -0500

    library, read and print errors fix

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -31,8 +31,8 @@ uint8_t Sym[32] = {0x3f,0x06,0x5b,0x4f,  //0,1,2,3
                    0x66,0x6d,0x7d,0x07,  //4,5,6,7
                    0x7f,0x6f,0x77,0x7c,  //8,9,A,b
                    0x39,0x5e,0x79,0x71,  //C,d,E,F
-                   0x00,0x72,0x50,0x5c,  // ,P,r,o
-                   0x30,0x30,0x54,0x4d,  //i,-,n,G
+                   0x00,0x73,0x50,0x5c,  // ,P,r,o
+                   0x30,0x40,0x54,0x4d,  //i,-,n,G
                    0x00,0x00,0x00,0x00,  //
                    0x00,0x00,0x00,0x00}; //
 
@@ -53,9 +53,12 @@ void render(uint8_t a, uint8_t b,  uint8_t c,  uint8_t d, uint8_t p ){
   TM1637_display_segments(3,Sym[d]);
 }
 
-uint8_t read_button(){
-  //r:read  pr:previous_r
-  uint8_t r,pr = 0x7;
+uint8_t read_button(uint8_t mode){
+  //modes:
+  //  0: return only after a key is pressed
+  //  1: return after a change
+  //r:read
+  uint8_t r = 0x00;
   //infinite loop until read a value
   while(1){
     //get the pin values isolated
@@ -73,38 +76,42 @@ uint8_t read_button(){
     //0x03 = z button pressed
     //0x02 = xz buttons pressed
     //0x01 = yz buttons pressed
-    if (pr != r){ 
-      pr = r;
-      switch (r){
-        case 0x6: //x button
-          return 0;
-        case 0x5: //y button
-          return 1;
-        case 0x3: //z button
-          return 2;
-      }
+    switch (r){
+      case 0x6: //x button
+        return 0;
+      case 0x5: //y button
+        return 1;
+      case 0x3: //z button
+        return 2;
+      default:
+        if(mode==1) return 3;
+        break;
     }
   }
 }
 
 uint8_t read_input(uint8_t limit){
-  uint8_t r = 0x00;
+  //r:read  pr:previous_r
+  uint8_t r,pr = 0x7;
   //op:selected_option
   uint8_t op = 0x00;
 
   while(1){
-    r = read_button(); 
-    switch (r){
-      case 0: //x button
-        if(op<=0) op=limit;
-        else op--;
-        break;
-      case 2: //z button
-        if(op>=limit) op=0x0;
-        else op++;
-        break;
-      case 1: //y button
-        return op;
+    r = read_button(1); //mode 1, return always 
+    if(r != pr){
+      pr = r;
+      switch (r){
+        case 0: //x button
+          if(op<=0) op=limit;
+          else op--;
+          break;
+        case 2: //z button
+          if(op>=limit) op=0x0;
+          else op++;
+          break;
+        case 1: //y button
+          return op;
+      }
     }
     render(0xf+4,0xf+2,byte_hi(op),byte_lo(op),1);
   }
@@ -150,7 +157,7 @@ int main (void){
         _delay_ms(2000);
         break;
       case 2: //Rock Paper Sissor
-        i = read_button();
+        i = read_button(0); //mode 0, wait for key press
         j = (random() % 3); //generate numbers in range [0,3)
         render(0xf+1,i,0xf+1,j,1);
         _delay_ms(2000);
