5fa9d3eb3004a7493bc7eef68da4d9c8da9c67e1
Oscar Bernal <oscarbernal93@gmail.com>
2021-05-21 17:29:23 -0500

    Adds expected results to check tool

--- calculator/check_rolls.c
+++ calculator/check_rolls.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DICE 100
+
 int main(){
   FILE *file;
   char *line = NULL;
@@ -16,7 +18,7 @@ int main(){
   int median = 0;
   int max = 0;
 
-  int nums[100] = {0};
+  int nums[DICE] = {0};
 
   file = fopen("dice_rolls.txt","r");
   if (file == NULL) exit(EXIT_FAILURE);
@@ -27,7 +29,7 @@ int main(){
     total += n;
     count++;
   }
-  for (i=0; i<100; i++){
+  for (i=0; i<DICE; i++){
     if(j<((count+1)/2)){
       median = i;
     }
@@ -39,20 +41,21 @@ int main(){
   }
 
   printf("Rolls:  %d\n",count);
-  printf("Total:  %d\n",total);
-  printf("Mean:   %d\n",(total/count));
-  printf("Median: %d\n",median);
-  printf("Mode:   %d\n",mode);
+  printf("Total:  %d/%d\n",total,(((DICE*(DICE+1))/2)*(count/100)));
+  printf("Mean:   %d/%d\n",(total/count),DICE/2);
+  printf("Median: %d/%d\n",median,DICE/2);
+  printf("Mode:   %d appears %d times\n",mode,max);
   for(i=max; i>=0; i--){
     k = 0;
-    for(j=0;j<100;j++) if (nums[j] == i) k++;
+    for(j=0;j<DICE;j++) if (nums[j] == i) k++;
     printf("%d Numbers have apeared %d times\n",k,i);
   }
   printf("Numbers that didn't appear: ");
-  for (i=0; i<100; i++){
+  for (i=0; i<DICE; i++){
     if (nums[i] == 0){
       printf("%d ",i);
     } 
   }
   printf("\n");
+  printf("We expect each number to appear %d times.\n",(count/DICE));
 }
